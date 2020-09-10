c675d12ce6259ca8e13e580ab75f309e0fb2eb1a
Rusakov Aleksey <rusal@bk.ru>
2020-09-10 19:19:45 +0300

    add tabl

--- main.py
+++ main.py
@@ -4,60 +4,26 @@ import numpy as np
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-#
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def sqrt2(x):
-
-    if x<=6:
-        return pow(x, 2)
-    elif x<=8:
-        return  x**3
-    elif x<=20:
-        return x
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # print(sqrt2(5))
-    # print(sqrt2(7))
-    # print(sqrt2(10))
-    # print(sqrt2(30))
-    # np.
-    # rows = []
-    # columns = []
-    # num = 0
-    # for i in range(2):
-    #     for j in range(6):
-    #         num += 1
-    #         columns.append(num)
-    #     rows.append(columns)
-    #     columns = []
-    # for i in range(0,2):
-    #     for j in range(0,6):
-    #         num+=1
-    #         print(num)
-    #         rows[i][j]=num
-    # print(F" {rows} \n \n \n ")
 
-    # cf=[5,4,5]
-    # for i in range(3):
-    #     cf[i]*=-1
-    # print(cf)
-    # tabl = np.zeros((4, 9))
-    # print(tabl)
-    mas=[]
-    for i in range(4):
-        row=[1,2,3]
-        mas.append(row)
-    mas.insert(1,[1,2,3,4])
 
-    print(F"\n  mas= {mas} \n")
-    z=range(5)
-    print(z)
+    cf =np.zeros((1,3))
+    ogr=np.zeros((3,4))
+    tabl = np.zeros((4, 9))
+    cf=[9,10,16]
+    ogr=[[18,15,12,360],
+         [6,4,8,192],
+         [5,3,3,180]]
+    for i in range(3):
+        tabl[3][2+i] = (-1) * cf[i]
+        tabl[i+1][0]=4+i;
 
+    print(cf)
+    print(ogr)
+
+    print(tabl)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
