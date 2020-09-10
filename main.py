6a532e15eb20f0df38a1fac9338349c6a0164083
Rusakov Aleksey <rusal@bk.ru>
2020-09-10 18:20:46 +0300

    add list 4 BISO (love)

--- main.py
+++ main.py
@@ -22,31 +22,42 @@ def sqrt2(x):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     # print(sqrt2(5))
     # print(sqrt2(7))
     # print(sqrt2(10))
     # print(sqrt2(30))
     # np.
-    x = 2
-    y = 6
-    rows = []
-    columns = []
-    num = 0
-    for i in range(2):
-        for j in range(6):
-            num += 1
-            columns.append(num)
-        rows.append(columns)
-        columns = []
+    # rows = []
+    # columns = []
+    # num = 0
+    # for i in range(2):
+    #     for j in range(6):
+    #         num += 1
+    #         columns.append(num)
+    #     rows.append(columns)
+    #     columns = []
     # for i in range(0,2):
     #     for j in range(0,6):
     #         num+=1
     #         print(num)
     #         rows[i][j]=num
-    print(F" {rows} \n \n \n ")
+    # print(F" {rows} \n \n \n ")
 
-    tabl = np.zeros((4, 9), )
-    tabl[1,1]="Petya"
-    print(tabl)
+    # cf=[5,4,5]
+    # for i in range(3):
+    #     cf[i]*=-1
+    # print(cf)
+    # tabl = np.zeros((4, 9))
+    # print(tabl)
+    mas=[]
+    for i in range(4):
+        row=[1,2,3]
+        mas.append(row)
+    mas.insert(1,[1,2,3,4])
+
+    print(F"\n  mas= {mas} \n")
+    z=range(5)
+    print(z)
+
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
